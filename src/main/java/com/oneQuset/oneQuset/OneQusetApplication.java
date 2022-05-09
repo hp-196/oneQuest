a30a997cf4d5fe0c235731911c701622c320262f
도넛맨 <hp196@naver.com>
2022-05-09 12:55:14 +0900

    Initial commit

---
+++ src/main/java/com/oneQuset/oneQuset/OneQusetApplication.java
@@ -0,0 +1,13 @@
+package com.oneQuset.oneQuset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneQusetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneQusetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneQuset/oneQuset/OneQusetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneQuset.oneQuset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneQusetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
