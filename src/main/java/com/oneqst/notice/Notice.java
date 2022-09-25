c6c5760e83f50b6dee3e99cff466d03a24c71e63
hp196@naver.com <hp196@naver.com>
2022-09-25 14:39:56 +0900

    알림기능 도메인 추가

---
+++ src/main/java/com/oneqst/notice/Notice.java
@@ -0,0 +1,34 @@
+package com.oneqst.notice;
+
+import com.oneqst.Member.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Notice {
+
+    @Id @GeneratedValue
+    @Column(name = "NOTICE_ID")
+    private Long id;
+
+    private String title; //알림 제목
+
+    private String content; //알림 내용
+
+    @ManyToOne
+    private Member member; //알림 멤버
+
+    @Enumerated(EnumType.STRING)
+    private NoticeType noticeType; //알림 타입
+
+    private LocalDateTime noticeTime; //알림시간
+
+
+
+
+}
---
+++ src/main/java/com/oneqst/notice/NoticeType.java
@@ -0,0 +1,12 @@
+package com.oneqst.notice;
+
+public enum NoticeType {
+    /**
+     * 알림 타입은
+     * 1.퀘스트에 초대받을경우
+     * 2.퀘스트 인증이 완료될경우
+     * 3.퀘스트 포스팅에 댓글이 달릴경우
+     */
+
+    QUEST_INVITE, QUEST_AUTH ,POST_COMMENT
+}
